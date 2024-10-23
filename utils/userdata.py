faf280353ce43233b5fc4b6eca257b2b36a438e0
infernostars <infernity@infernity.dev>
2024-10-22 19:50:33 -0600

    minor qol changes

--- utils/userdata.py
+++ utils/userdata.py
@@ -16,16 +16,16 @@ GUILD_AVAILABLE_DATA: Dict[str, Property[Any]] = {
 GLOBAL_DATA: Dict[str, Property[Any]] = {
 }
 
-class DataManager(PersistentStorage):
+class IdentifierDataManager(PersistentStorage):
     def __init__(self, entity_type: str, entity_id: Union[int, str], available_data: Dict[str, Property]):
         file_path = f'data/{entity_type}s/{entity_id}.json'
         super().__init__(available_data, file_path)
 
-class UserDataManager(DataManager):
+class UserDataManager(IdentifierDataManager):
     def __init__(self, user_id: int):
         super().__init__("user", user_id, USER_AVAILABLE_DATA)
 
-class GuildDataManager(DataManager):
+class GuildDataManager(IdentifierDataManager):
     def __init__(self, guild_id: int):
         super().__init__("guild", guild_id, GUILD_AVAILABLE_DATA)
 
@@ -33,8 +33,8 @@ class GlobalDataManager(PersistentStorage):
     def __init__(self):
         super().__init__(GLOBAL_DATA, "data/global.json")
 
-def get_data_manager(entity_type: Literal["user", "guild", "global"], entity_id: int) -> DataManager:
-    """Returns a DataManager based on entity type and entity id."""
+def get_data_manager(entity_type: Literal["user", "guild", "global"], entity_id: int) -> PersistentStorage:
+    """Returns a PersistentStorage instance based on entity type and entity id."""
     if entity_type == "user":
         return UserDataManager(entity_id)
     elif entity_type == "guild":
